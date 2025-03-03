db2004273b0aaeb2226a5e0b41158bab81adad03
Joydip Das <190102.cse@student.just.edu.bd>
2025-03-03 14:52:06 +0600

    backend added

--- sharif_backend/urls.py
+++ sharif_backend/urls.py
@@ -1,35 +1,14 @@
-"""
-URL configuration for sharif_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-
-# sharif_backend/urls.py
-from django.contrib import admin
-from django.urls import path, include
 
 from django.contrib import admin
 from django.urls import path, include
-from sharif_backend import views  # Import views from the sharif_backend app
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('api/auth/', include('Authentication.urls')),  # Include the Authentication app URLs
-    path('', views.home),  # Root path should now point to the home view
+    path('api/', include('Authentication.urls')),
+    path('api/',include('master.urls')),
+    path('api/',include('Inventory.urls')),
 ]
-
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
